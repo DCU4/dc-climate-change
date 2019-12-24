a36db872d1e5ab53e49d84a12e0b32decf663e70
Dylan Connor <DylanJConnor4@gmail.com>
2019-12-24 11:44:59 -0500

    testing noaa api calls

--- src/js/components/container/Container.jsx
+++ src/js/components/container/Container.jsx
@@ -15,7 +15,9 @@ class Container extends Component {
     getData = async e => {
       const dc = 'FIPS:11';
       const token = 'CsgMDhKauNyJczMlffoEibhLIPkiQXDj';
-      const datatypeCall = await fetch(`https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?locationcategoryid=${dc}&datacategoryid=TEMP&limit=1000`,{
+      // const url = `https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?locationcategoryid=${dc}&datacategoryid=TEMP&limit=1000`;
+      const url = `https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?locationid=${dc}`;
+      const datatypeCall = await fetch(url,{
         headers :{
           token: token
         }
@@ -23,17 +25,20 @@ class Container extends Component {
       const datatype = await datatypeCall.json();
       console.log('dataype:',datatype);
 
-
-      const datasetCall = await fetch(`https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?datatypeid=${datatype.results[55].id}`,{
+      // `https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?datatypeid=${datatype.results[55].id}`
+      const datasetCall = await fetch(`https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?stationid=${datatype.results[19].id}`,{
         headers :{
           token: token
         }
       });
       const dataset = await datasetCall.json();
       console.log('dataset:',dataset);
+
+
+
       if (dataset){
         console.log('getting data...')
-        const call = await fetch(`https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=${dataset.results[0].id}&startdate=2019-12-19&enddate=${dataset.results[0].maxdate}&units=standard&limit=1000`,
+        const call = await fetch(`https://www.ncdc.noaa.gov/cdo-web/api/v2/data?stationid=${datatype.results[19].id}&datasetid=${dataset.results[0].id}&startdate=2019-01-01&enddate=2019-01-31&units=standard&limit=1000&datatype=PRCP`,
           {
           headers :{
             token: token
@@ -43,13 +48,7 @@ class Container extends Component {
         const data = await call.json();
         console.log('data', data, call);
       }
-      // if(data){
 
-      //   // this.setState({
-      //   //   dollar: data.rates.USD,
-      //   //   euro: data.rates.EUR
-      //   // });
-      // }
     }
 
     componentDidMount () {
