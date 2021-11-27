60ae3cc0cac686c3a61bbd467c8b15451439a232
Dylan Connor <DylanJConnor4@gmail.com>
2021-11-27 18:03:54 -0500

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,9 @@ module.exports = {
       {
         test:/\.(s*)css$/,
         use: [
-
           "style-loader", // creates style nodes from JS strings
           "css-loader", // translates CSS into CommonJS
           "sass-loader"
-
         ]
       }
     ]
