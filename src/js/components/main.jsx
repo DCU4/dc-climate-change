3ce73661f6d7cb1cd2e009d118daaaad431956c9
Dylan Connor <DylanJConnor4@gmail.com>
2025-09-27 17:48:36 -0400

    styles

--- src/js/components/main.jsx
+++ src/js/components/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Container from '../components/container/Container.jsx'
-// src/js/components/container/Container.jsx
-// import '../../styles/main.scss'
+import '../../styles/style.scss'
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(
