da8ed8538fad7fbd5c6ac21b29c402d5d932676a
Dylan Connor <DylanJConnor4@gmail.com>
2025-09-28 16:30:50 -0400

    run build; update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+function moveScriptsToFooter() {
+  return {
+    name: 'move-scripts-to-footer',
+    transformIndexHtml(html) {
+      // Move all <script type="module"> tags before </body>
+      // Remove all <script type="module"> tags and collect them
+      const scripts = [];
+      const htmlWithoutScripts = html.replace(/<script type="module"[\s\S]*?<\/script>/g, (script) => {
+        scripts.push(script);
+        return '';
+      });
+      // Insert collected scripts just before </body>
+      return htmlWithoutScripts.replace(
+        /(<\/body>)/i,
+        scripts.join('\n') + '\n$1'
+      );
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  base: '/dc-climate-change/',
+  plugins: [react(), moveScriptsToFooter()],
   build: {
     root: 'src',
-    // emptyOutDir: true,
     outDir: 'docs'
   }
 })
